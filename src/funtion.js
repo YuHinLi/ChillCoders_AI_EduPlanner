41f11099889b2db3cc22d43cb62071f78719eb6a
YuHinLi <yuhinl@student.unimelb.edu.au>
2024-04-04 22:05:36 +1100

    Initial Commit

---
+++ src/funtion.js
@@ -0,0 +1,88 @@
+function updateEndTimeOptions() {
+    var startTime = document.getElementById('startTime').value;
+    var endTime = document.getElementById('endTime');
+    var timeOptions = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', '9pm', '10pm', '11pm'];
+    var startIndex = timeOptions.indexOf(startTime);
+
+    // Clear existing options in endTime
+    while (endTime.options.length > 0) {
+        endTime.remove(0);
+    }
+
+    // Populate endTime with times after the selected startTime
+    for (var i = startIndex + 1; i < timeOptions.length; i++) {
+        var option = document.createElement('option');
+        option.value = timeOptions[i];
+        option.text = timeOptions[i];
+        endTime.add(option);
+    }
+}
+
+function showNext(nextSectionId) {
+    var currentSection = document.getElementById(nextSectionId).previousElementSibling;
+    currentSection.classList.add('hidden');
+    document.getElementById(nextSectionId).classList.remove('hidden');
+    manageBackButtonVisibility(nextSectionId);
+}
+
+function showPrevious(previousSectionId) {
+    var allSections = document.querySelectorAll('.section');
+    allSections.forEach(function(section) {
+        section.classList.add('hidden');
+    });
+
+    var previousSection = document.getElementById(previousSectionId);
+    previousSection.classList.remove('hidden');
+    manageBackButtonVisibility(previousSectionId);
+
+    if (previousSectionId === 'subjectSection') {
+        resetSubjectSection();
+    }
+}
+
+function manageBackButtonVisibility(currentSectionId) {
+    var backButtons = document.querySelectorAll('.backBtn');
+    backButtons.forEach(function(btn) {
+        btn.classList.add('hidden');
+    });
+    if (currentSectionId !== 'studyDurationSection') {
+        var currentSection = document.getElementById(currentSectionId);
+        var backButton = currentSection.querySelector('.backBtn');
+        if (backButton) {
+            backButton.classList.remove('hidden');
+        }
+    }
+}
+
+function resetSubjectSection() {
+    var subjectInputs = document.getElementById('subjectInputs');
+    subjectInputs.innerHTML = '';
+}
+
+
+function addSubject() {
+    var subjectIndex = document.querySelectorAll('.subject').length;
+    var subjectDiv = document.createElement('div');
+    subjectDiv.className = 'subject';
+    subjectDiv.innerHTML =
+        '<label>Subject Name:</label>' +
+        '<input type="text" name="subjectName[]"><br>' +
+        '<div class="tasks" id="tasks' + subjectIndex + '"></div>' +
+        '<button type="button" onclick="addTask(' + subjectIndex + ')">Add Task</button><br>';
+    document.getElementById('subjectInputs').appendChild(subjectDiv);
+}
+
+function addTask(subjectIndex) {
+    var taskDiv = document.createElement('div');
+    taskDiv.innerHTML =
+        '<label>Task:</label>' +
+        '<input type="text" name="subjectTask' + subjectIndex + '[]"><br>' +
+        '<label>End Date for Task (optional):</label>' +
+        '<input type="date" name="subjectTaskEndDate' + subjectIndex + '[]"><br>';
+    document.getElementById('tasks' + subjectIndex).appendChild(taskDiv);
+}
+
+document.getElementById('studyPlanForm').onsubmit = function(event) {
+    event.preventDefault();
+    alert('Form submitted. Implement AJAX or form submission logic here.');
+};
